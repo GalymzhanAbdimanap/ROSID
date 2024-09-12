ace2e15da72ffa6190b3983c1216c6ca37d01fb9
modernToking_ <33455244+GalymzhanAbdimanap@users.noreply.github.com>
2024-09-12 17:20:00 +0500

    Update oil_spill_320_320.py

--- segmentation/configs/_base_/datasets/oil_spill_320_320.py
+++ segmentation/configs/_base_/datasets/oil_spill_320_320.py
@@ -1,5 +1,5 @@
 dataset_type = 'OilSpillDataset'
-data_root = '../landsat/dataset_landsat_743_320_320'
+data_root = './ROSID_320_320'
 crop_size = (320, 320)
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
